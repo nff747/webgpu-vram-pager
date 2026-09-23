88904967504341df8291baa0ed2c8edc72a4f90f
iKi <180210171+nff747@users.noreply.github.com>
2026-09-23 20:44:07 -0300

    add test for defragment

--- tests/VRAMAllocator.test.ts
+++ tests/VRAMAllocator.test.ts
@@ -24,4 +24,20 @@ describe('VRAMAllocator', () => {
     const alloc3 = allocator.allocate(128);
     expect(alloc3.offset).toBe(0);
   });
+
+  it('should defragment memory correctly', () => {
+    const allocator = new VRAMAllocator(1024);
+    const alloc1 = allocator.allocate(256);
+    const alloc2 = allocator.allocate(256);
+    const alloc3 = allocator.allocate(256);
+    
+    allocator.free(alloc2.id);
+    
+    // Memory map: [alloc1 (0-256)] [FREE (256-512)] [alloc3 (512-768)]
+    
+    allocator.defragment();
+    
+    // Memory map should now be: [alloc1 (0-256)] [alloc3 (256-512)]
+    expect(alloc3.offset).toBe(256);
+  });
 });
