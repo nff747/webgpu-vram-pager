e913dfd6d26ea06d6370c7f4cc1f254c775e541b
iKi <180210171+nff747@users.noreply.github.com>
2026-09-23 20:44:54 -0300

    add index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export { PagerEngine, PagerEngine as VRAMPager } from './core/PagerEngine';
-export { WebGPUContext } from './core/WebGPUContext';
-export { WeightStreamer } from './streaming/WeightStreamer';
-export { ComputePipeline } from './compute/ComputePipeline';
-export type * from './types';
-export { RingBufferPool, RingBufferPool as RingBuffer, RingBufferPool as MemoryPool } from './core/RingBuffer';
+export * from './types';
+export * from './VRAMAllocator';
